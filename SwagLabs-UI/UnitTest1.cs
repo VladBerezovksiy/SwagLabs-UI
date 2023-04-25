5cbf92c6cd8a9a52f13d65ec7533bc3e1b581452
VladyslavBerezovskyi <VladBerezovksiy>
2023-04-25 20:08:56 +0300

    Add project files.

---
+++ SwagLabs-UI/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace SwagLabs_UI
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
